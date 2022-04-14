7d4a4f3380c74196fa86ec8694ac49f39c0504f9
JohnRyanJordan <john.ryan.jordan@gmail.com>
2022-04-14 13:34:49 -0500

    Added initial script and readme

---
+++ README.md
@@ -0,0 +1,61 @@
+# Dev Configurator Instructions
+
+1. Open terminal
+2. Navigate terminal to this folder: `cd ~/path/dev-configurator`
+3. Run command: `./init.sh` to start the install script
+
+
+  **\*Note**: If unable to run script run the command: `chmod +x ~/path/dev-configurator/init.sh` to allow execution of the install script 
+
+
+4. Let the process run. Near the end there are a few y/n (yes or no) prompts. Press key y/n to accept or not
+5. After the script is finished you should be able to use the Mac search (top right) and see the following installed:
+   - **\*Note**: If any of these are missing run the command in parenthesis\*
+   - iTerm (`brew cask install iterm`)
+   - Google Chrome (`brew cask install google-chrome`)
+   - Slack (`brew cask install slack`)
+   - [Optional] VS Code (`brew cask install visual-studio-code`)
+6. Finally, you may need to restart your machine to ensure all updates take effect
+
+### What's included
+
+- Git command line tools (Xcode)
+- Homebrew package manager
+- NodeJS
+- Yarn (global)
+- Gulp (global)
+- Bower (global)
+- Multiple linter packages (global)
+  - stylelint
+  - eslint
+  - prettier
+  - stylelint-scss
+  - stylelint-order
+  - eslint-config-airbnb
+  - eslint-config-prettier
+  - eslint-config-airbnb-base
+  - eslint-plugin-prettier
+  - eslint-plugin-import
+  - eslint-plugin-jsx-a11y
+  - eslint-plugin-react
+- Multiple cli packages (global)
+  - Serverless
+  - CRA (Create React App)
+  - Expo
+  - eas
+  - Amplify (AWS)
+- Multiple desktop applications
+  - iTerm
+  - Google Chrome
+  - Microsoft Teams
+  - Discord
+  - Slack
+  - Android Studio
+  - Android Platform tools (adb)
+  - Github
+  - [Optional] VS Code
+  - [Optional] Atom.io
+- Oh My Zsh (Z Shell)
+
+
+**Inspiration from [Dlerm's dev-box.sh](https://gist.github.com/dlerm/c9bd9eaab46a3024481eb803477f4ed2)**
---
+++ init.sh
@@ -0,0 +1,172 @@
+#!/usr/bin/env bash
+
+# Install Xcode
+# Install Homebrew
+# Install NVM
+# Install Yarn
+# Install Gulp
+# Install Bower
+# Install Linters
+# Install Serverless
+# Install Create React App
+# Install Expo
+# Install eas
+# Install AWS Amplify
+# Set OS defaults
+# Install Desktop Applications
+# Setup Projects Folder
+# Install Oh-My-Zsh
+
+# Ask for the administrator password upfront
+sudo -v
+
+echo "------------------------------"
+echo "Initializing Development Workspace [×]"
+
+# Update OS
+echo "Updating OSX.  If this requires a restart, run the script again."
+# # Install all available updates
+# sudo softwareupdate -ia --verbose
+# # Install only recommended available updates
+sudo softwareupdate -ir --verbose
+
+# Install Xcode Tools (Git)
+
+echo "Installing Xcode Command Line Tools..."
+# Install Xcode command line tools
+xcode-select --install
+
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing Homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+# Make sure we’re using the latest Homebrew.
+brew update
+
+
+# # Install Node & Global Packages
+# echo "Installing Node..."
+# brew install node
+
+echo "Installing NVM..."
+brew install nvm
+mkdir ~/.nvm 
+echo -n 'export NVM_DIR=~/.nvm source $(brew --prefix nvm)/nvm.sh' >> ~/.zshrc
+source ~/.zshrc
+
+nvm install 14
+nvm use 14
+
+echo "Installing Yarn..."
+brew install yarn
+
+
+# echo "Installing Gulp..."
+# npm install -g gulp-cli
+
+# echo "Installing Bower..."
+# npm install -g bower
+
+# Install Linters
+echo "Installing Linters..."
+npm install -g stylelint
+npm install -g eslint
+npm install -g prettier
+npm install -g -D stylelint-scss
+npm install -g -D stylelint-order
+npm install -g -D eslint-config-airbnb
+npm install -g -D eslint-config-prettier
+npm install -g -D eslint-config-airbnb-base
+npm install -g -D eslint-plugin-prettier
+npm install -g -D eslint-plugin-import 
+npm install -g -D eslint-plugin-jsx-a11y 
+npm install -g -D eslint-plugin-react 
+
+echo "Installing Serverless..."
+npm install -g serverless
+
+echo "Installing Create React App..."
+npm install -g create-react-app
+
+echo "Installing Expo cli..."
+npm install -g expo-cli
+
+echo "Installing eas cli..."
+npm install -g eas-cli
+
+echo "Installing Amplify cli..."
+npm install -g @aws-amplify/cli
+
+# Set OS Defaults
+echo "Setting OS Defaults..."
+# Finder: show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# Disable smart quotes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Disable press-and-hold for keys in favor of key repeat
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+
+# Install Desktop Applications
+echo "Installing iTerm2..."
+brew cask install iterm2
+
+echo "Installing Google Chrome..."
+brew cask install google-chrome
+
+echo "Installing Microsoft Teams..."
+brew cask install microsoft-teams
+
+echo "Installing Discord..."
+brew cask install discord
+
+echo "Installing Slack..."
+brew cask install slack
+
+echo "Installing Android Studio..."
+brew cask install android-studio
+
+echo "Installing Android Studio adb tools..."
+brew cask install android-platform-tools
+
+echo "Installing Github..."
+brew cask install github
+
+echo "Installing AWS CLI..."
+brew install awscli
+
+read -p "Would you like to download VSCode? (y/n) " -n 1;
+echo "";
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  echo "Installing VSCode..."
+  brew cask install visual-studio-code
+fi;
+
+read -p "Would you like to download Atom? (y/n) " -n 1;
+echo "";
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  echo "Installing Atom..."
+  brew cask install atom
+fi;
+
+# Install Oh My Zsh
+echo "Installing Oh My Zsh (Z shell)..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+
+echo "Development Workspace Ready [✔]"
+echo ""
+echo "Restart your computer to ensure all updates take effect"
+echo "------------------------------"
